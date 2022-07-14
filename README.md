86adbcc3c7a295e7aa6e9a894ac1dcebc1ac3905
Alexander Trost <galexrt@googlemail.com>
2022-07-15 00:00:25 +0200

    rewrite griefdefender api usage
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

---
+++ README.md
@@ -0,0 +1,5 @@
+# Insights GriefDefender Addon
+
+An addon for [Insights](https://www.spigotmc.org/resources/insights-super-configurable-region-limits-asynchronous-scans-1-19.56489/) to limit [GriefDefender](https://www.spigotmc.org/resources/1-12-2-1-19-griefdefender-claim-plugin-grief-prevention-protection.68900/) plots.
+
+Based on the [InsightsPlugin/GriefPreventionAddon](https://github.com/InsightsPlugin/GriefPreventionAddon) code.
---
+++ src/main/java/moe/galexrt/insights/addons/griefdefender/GriefDefenderAddon.java
@@ -0,0 +1,91 @@
+package moe.galexrt.insights.addons.griefdefender;
+
+import dev.frankheijden.insights.api.InsightsPlugin;
+import dev.frankheijden.insights.api.addons.InsightsAddon;
+import dev.frankheijden.insights.api.addons.Region;
+import dev.frankheijden.insights.api.addons.SimpleCuboidRegion;
+import dev.frankheijden.insights.api.objects.math.Vector3;
+import net.kyori.event.method.annotation.Subscribe;
+
+import com.flowpowered.math.vector.Vector3i;
+import com.griefdefender.api.GriefDefender;
+import com.griefdefender.api.claim.Claim;
+import com.griefdefender.api.event.ChangeClaimEvent;
+import com.griefdefender.api.event.RemoveClaimEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.event.Listener;
+import java.util.Optional;
+
+public class GriefDefenderAddon implements InsightsAddon, Listener {
+
+    public String getId(Claim claim) {
+        return getPluginName() + "@" + claim.getUniqueId();
+    }
+
+    public GriefDefenderAddon() {
+        // Register our GriefDefender events listeners just when the server started
+        Bukkit.getScheduler().runTask(Bukkit.getPluginManager().getPlugin("Insights"), () -> {
+            GriefDefender.getEventManager().register(this);
+        });
+    }
+
+    public Optional<Region> adapt(Claim claim) {
+        if (claim == null)
+            return Optional.empty();
+
+        Vector3i minVector = claim.getLesserBoundaryCorner();
+        Vector3i maxVector = claim.getGreaterBoundaryCorner();
+
+        World world = Bukkit.getWorld(claim.getWorldUniqueId());
+        Location min = new Location(world, minVector.getX(), minVector.getY(), minVector.getZ());
+        Location max = new Location(world, maxVector.getX(), maxVector.getY(), maxVector.getZ());
+
+        return Optional.of(new SimpleCuboidRegion(
+                min.getWorld(),
+                new Vector3(min.getBlockX(), 0, min.getBlockZ()),
+                new Vector3(max.getBlockX(), max.getWorld().getMaxHeight() - 1, max.getBlockZ()),
+                getPluginName(),
+                getId(claim)));
+    }
+
+    @Override
+    public String getPluginName() {
+        return "GriefDefender";
+    }
+
+    @Override
+    public String getAreaName() {
+        return "claim";
+    }
+
+    @Override
+    public String getVersion() {
+        return "{version}";
+    }
+
+    @Override
+    public Optional<Region> getRegion(Location location) {
+        return adapt(GriefDefender.getCore().getClaimManager(location.getWorld().getUID()).getClaimAt(location.getBlockX(),
+                location.getBlockY(), location.getBlockZ()));
+    }
+
+    @Subscribe
+    public void onChangeClaimEvent(ChangeClaimEvent event) {
+        if (event.cancelled()) return;
+
+        deleteClaimCache(event.getClaim());
+    }
+
+    @Subscribe
+    public void onRemoveClaimEvent(RemoveClaimEvent event) {
+        if (event.cancelled()) return;
+
+        deleteClaimCache(event.getClaim());
+    }
+
+    private void deleteClaimCache(Claim claim) {
+        InsightsPlugin.getInstance().getAddonStorage().remove(getId(claim));
+    }
+}
